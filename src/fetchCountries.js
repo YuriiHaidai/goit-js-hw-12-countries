5a7e31ea3c5537d58aeb22fd63a51f2bbaafcd42
YuriiHaidai <ura.kiev@gmail.com>
2020-03-30 21:34:54 +0300

    firs commite

---
+++ src/fetchCountries.js
@@ -0,0 +1,3 @@
+// Есть файл fetchCountries.js с дефолтным экспортом функции fetchCountries(searchQuery), возвращающей промис с массивом стран, результат запроса к API.
+
+fetch('https://restcountries.eu/rest/v2/name/{name}');
