e62ae2293acec392ec3ab43299fb461abd871c3b
Gaiday_u <ura.kiev@gmail.com>
2020-03-31 19:15:10 +0300

    fix4

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,1 +1,5 @@
-// Есть файл fetchCountries.js с дефолтным экспортом функции fetchCountries(searchQuery), возвращающей промис с массивом стран, результат запроса к API.
+export default function fetchCountries(searchQuery) {
+  return fetch(
+    `https://restcountries.eu/rest/v2/name/${searchQuery}`,
+  ).then(response => response.json());
+}
--- src/index.js
+++ src/index.js
@@ -1,57 +1,33 @@
 import './styles.css';
-// import './fetchCountries';
+import fetchCountries from './fetchCountries';
 import PNotify from 'pnotify/dist/es/PNotify.js';
+import singleCountrie from './templates/singleCountrie.hbs';
+import contriesList from './templates/countriesList.hbs';
+
 const debounce = require('lodash.debounce');
 
 const list = document.querySelector('.list');
 const input = document.querySelector('.input');
 
 function findCountries(event) {
-  const value = event.target.value;
-  fetch(`https://restcountries.eu/rest/v2/name/${value}`)
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      console.log(data);
+  const inputValue = event.target.value;
+  if (inputValue) {
+    fetchCountries(inputValue).then(data => {
       if (data.length === 1) {
-        const markup = data
-          .map(
-            ({
-              name,
-              capital,
-              population,
-              languages,
-              flag,
-            }) => `<h2 class="name">${name}</h2>
-        <div class="info">
-          <div class="info--text">
-            <p class="capital"><strong>Capital: </strong>${capital}</p>
-            <p class="population"><strong>Population: </strong>${population}</p>
-            <p class="languages"><strong>Languages: </strong></p>
-            <ul class="languages-list">
-              {{#each languages}}
-              <li class="language">{{this.name}}</li>
-              {{/each}}
-            </ul>
-            </div>
-            <img src="${flag}" alt="flag of "${name}" class="flag" />
-        </div>`,
-          )
-          .join('');
-        list.innerHTML = markup;
+        list.innerHTML = singleCountrie(data[0]);
         return;
       } else if (data.length > 1 && data.length < 11) {
-        const markup = data.map(({ name }) => `<li>${name}</li>`).join('');
-        list.innerHTML = markup;
+        list.innerHTML = contriesList(data);
         return;
       } else if (data.length >= 11) {
         PNotify.error({
           text: 'Too many matches found, enter more specific query',
-          delay: 5000,
+          delay: 500,
         });
       }
     });
+  }
+  list.innerHTML = '';
 }
 
 input.addEventListener('input', debounce(findCountries, 500));
