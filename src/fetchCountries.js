06e4a2a68162b2cccd67c799e1be977cd475a61f
Gaiday_u <ura.kiev@gmail.com>
2020-03-31 15:11:57 +0300

    fix2

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,3 +1,1 @@
 // Есть файл fetchCountries.js с дефолтным экспортом функции fetchCountries(searchQuery), возвращающей промис с массивом стран, результат запроса к API.
-
-fetch('https://restcountries.eu/rest/v2/name/{name}');
--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './styles.css';
+// import './fetchCountries';
+
+const list = document.querySelector('.list');
+
+fetch('https://restcountries.eu/rest/v2/name/south')
+  .then(response => {
+    console.log(response);
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+    const markup = data.map(
+      ({ name, capital }) => `<li><h2>${name}</h2><p>${capital}</p></li>`,
+    );
+    list.innerHTML = markup;
+  });
